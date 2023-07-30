33cd593b8bb539f1729e642cfe723a2641d50e37
ShubhamShree1320 <shubham.1320@gmail.com>
2023-07-30 21:44:38 +0530

    my first ts project

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+function swap(a, b) {
+    return [b, a];
+}
+let var1 = swap(1, 'a');
---
+++ index.ts
@@ -0,0 +1,7 @@
+function swap <T,U> (a:T,b:U):[U,T]{
+    
+    return [b,a];
+
+}
+
+let var1= swap(1,'a');
